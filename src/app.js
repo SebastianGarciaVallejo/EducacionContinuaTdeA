ba1d042793fde20f85324275ab5280d4df6e5c6e
SebastianGarciaVallejo <sebastiangarcia9201@gmail.com>
2019-03-30 19:00:25 -0500

    Pagina de inicio

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs = require('hbs');
 
+const directoriopublico = path.join(__dirname, '../public');
+const directoriopartials = path.join(__dirname, '../template/partials');
+const dirNode_modules = path.join(__dirname , '../node_modules');
 
+hbs.registerPartials(directoriopartials);
+
+app.use(express.static(directoriopublico));
+//app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('../template/views/index.hbs');
+});
+
+app.get('*', (req, res) => {
+    res.render('../template/views/error.hbs');
 });
 
 app.listen(3000, () => {
