ff8c062cfe8c86f96f0772c2a798298a4cdad623
Juan Carvajal <jecarvajal@sura.com.co>
2019-04-08 02:34:32 -0500

    Testing the app in heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
-const port = process.env.port || 3000;
+//const port = process.env.port || 3000;
 require('./helpers/helper.js')
 
 const directoriopublico = path.join(__dirname, '../public');
@@ -29,10 +29,11 @@ app.get('/', (req, res) => {
 app.get('/registroUsuario', (req, res) => {
     res.render('../template/views/registroUsuario.hbs');
 });
-
+/*
 app.listen(port,() => {
     console.log('Servidor en el puerto ' + port);
 });
+*/
 app.post('/resultadoOperacion', (req, res) => {
     res.render('../template/views/resultadoOperacion.hbs',{
         usuario:{
